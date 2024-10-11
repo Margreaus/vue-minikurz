74125d75308785e4185d7f42b5a66d3ee602919e
Student Student <student@primakurzy.cz>
2024-10-11 13:51:45 +0200

    zaklady vue

---
+++ zoo/src/components/UkolOdpocitavadlo.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+import {ref} from "vue"
+
+
+const pocet = ref(10);
+
+
+setInterval(() => {
+    //kdykoliv budeme chtit cist nebo menit hodnotu reactivni promenne, tak musime jit pres vlasntost value
+    pocet.value = Math.floor(Math.random() * 10000);
+}, 2000);
+
+</script>
